4e74205cee286468922d2800c93c5e8a82c1d714
qxzcode <qxzcraft@gmail.com>
2016-02-16 17:09:15 -0800

    Now increments with transactions; cleaned up code

--- index.js
+++ index.js
@@ -22,140 +22,118 @@ var globalInput = "";
 // Pass in parameters to the REST API using an object literal notation. The
 // REST client will handle authentication and response serialzation for you.
 http.createServer(function(request, response) {
-	console.log("GOT REQUEST AT \""+request.url+"\"");
 	
-    response.writeHead(700, {
-        "Content-Type": "text/bold"
-    });
-
-   
-    var parts = u.parse(request.url, true);
-    var query = parts.query;
-    var input = query.Body;
-    
-    var userNumber = query.From;
-    var reply = "";
-    var count = 0;
-
+	response.writeHead(700, {
+		"Content-Type": "text/bold"
+	});
+	
+	var parts = u.parse(request.url, true);
+	var query = parts.query;
+	var input = query.Body;
+	
+	var userNumber = query.From;
+	var reply = "";
+	var count = 0;
+	
     /*
         takes the Wikipedia reply and sends it back to user
     */
-    function respondToUser(reply,input) {
-        addDataToFireBase(input);
-
-        count++;
-
-        if (count < 2) {
-            client.sendMessage({
-
-                to: userNumber,
-                from: "+1844-743-9454",
-                body: reply
-
-            });
-            //addDataToFireBase(input);
-        }
-        else {
-            count = 0;
-        }
-    }
-
-
-
+	function respondToUser(reply,input) {
+		addDataToFireBase(input);
+		
+		count++;
+		
+		if (count < 2) {
+			client.sendMessage({
+				
+				to: userNumber,
+				from: "+1844-743-9454",
+				body: reply
+				
+			});
+			//addDataToFireBase(input);
+		} else {
+			count = 0;
+		}
+	}
+	
+	
+	
     /*
         if statement prevents double loop from occuring with a break in the console 
     */
-
-    if (input == undefined) {
-        return;
-    }
-
-    console.log("User input: " + input);
-
-
+	
+	if (input == undefined) {
+		return;
+	}
+	
+	console.log("User input: " + input);
+	
+	
     /*
         takes input, web scrapes Wikipedia, and returns first paragraph of page
     */
-
-    var url = "https://en.wikipedia.org/wiki/" + input;
-    req(url, function(error, response, body) {
-            
-    var $ = cheerio.load(body);
-    var firstPar = "";
-    if (!error) {
-    
-        if(input == "HELLO") {
-           firstPar = "Hi, I'm WikiText. Thanks for stopping by. You can look anything you want up just by texting this number with whatever you want to know about. You should get a reply back in under 30 seconds unless our server is down. Try to type in nouns and single words or phrases. Text \"TIPS\" for more help";
-           console.log(firstPar);
-           reply = firstPar;
-               
-        } else if (input == "TIPS") {
-           firstPar = "In case your having trouble, try searching just nouns or phases. If that doesn't work, then take a look at your capitalization (i.e. Dj Khaled won't work, but DJ Khaled will) and if that, too doesn't work, try putting underscores (_) instead of spaces. If none of those work, then there is no wikipedia page for this topic.";
-           console.log(firstPar);
-        } else {
-        
+	
+	var url = "https://en.wikipedia.org/wiki/" + input;
+	req(url, function(error, response, body) {
+	
+	var $ = cheerio.load(body);
+	var firstPar = "";
+	if (!error) {
+		
+		if(input.toUpperCase() == "HELLO") {
+			firstPar = "Hi, I'm WikiText. Thanks for stopping by. You can look anything you want up just by texting this number with whatever you want to know about. You should get a reply back in under 30 seconds unless our server is down. Try to type in nouns and single words or phrases. Text \"TIPS\" for more help";
+			console.log(firstPar);
+			reply = firstPar;
+		} else if (input.toUpperCase() == "TIPS") {
+			firstPar = "In case your having trouble, try searching just nouns or phases. If that doesn't work, then take a look at your capitalization (i.e. Dj Khaled won't work, but DJ Khaled will) and if that, too doesn't work, try putting underscores (_) instead of spaces. If none of those work, then there is no wikipedia page for this topic.";
+			console.log(firstPar);
+		} else {
+			
             if (!error) {
-    
-                firstPar = "" + $("#mw-content-text").find('p').eq(1);
-                firstPar = firstPar.replace(/<(?:.|\n)*?>/gm, '');
-                firstPar = firstPar.replace(/<[^>]*?>/gm, '');
-                firstPar = firstPar.replace(/&quot;/g, "\"");
-                firstPar = firstPar.replace(/\[(?:.|\n)*?\]/g, "");
-                firstPar = firstPar.replace(/&#x2013;/g, "--");
-                firstPar = firstPar.replace(/&#x2014;/g, "--");
-                firstPar = firstPar.replace(/&apos;/g, "\'");
-                firstPar = firstPar.replace(/&#xE9;/g, "é");
-                reply = firstPar;
-            } 
-        }
-            
-        if(firstPar.length !== 0) {
-            reply = firstPar;
-        } else {
-            reply = "Try again, we can't determine what '" + input + "' is. Try making your entry more specific.";
-            //alex came up with this vv
-             usersRef.child("FailedWords").push().set({
-            "words": input
-            });
-       
-            console.log(input + " did not work!");
-        }
-    } 
-        respondToUser(reply, input);
-        
-    });
-    
-    response.end();
+				firstPar = "" + $("#mw-content-text").find('p').eq(1);
+				firstPar = firstPar.replace(/<(?:.|\n)*?>/gm, '');
+				firstPar = firstPar.replace(/<[^>]*?>/gm, '');
+				firstPar = firstPar.replace(/&quot;/g, "\"");
+				firstPar = firstPar.replace(/\[(?:.|\n)*?\]/g, "");
+				firstPar = firstPar.replace(/&#x2013;/g, "--");
+				firstPar = firstPar.replace(/&#x2014;/g, "--");
+				firstPar = firstPar.replace(/&apos;/g, "\'");
+				firstPar = firstPar.replace(/&#xE9;/g, "é");
+				reply = firstPar;
+			}
+		}
+		
+		if (firstPar.length !== 0) {
+			reply = firstPar;
+		} else {
+			reply = "Try again, we can't determine what '" + input + "' is. Try making your entry more specific.";
+			//alex came up with this vv
+			usersRef.child("FailedWords").push({
+				"words": input
+			});
+			
+			console.log(input + " did not work!");
+		}
+	}
+		respondToUser(reply, input);
+		
+	});
+	
+	response.end();
 
 }).listen(process.env.PORT || 5000);
 
-console.log("Port: "+(process.env.PORT || 5000));
+console.log("Server listening on port: "+(process.env.PORT || 5000));
 
 
 /*
     add data to server so we can upload it to our website
 */
-
 function addDataToFireBase(word) {
-    
-    
-   usersRef.once("value", function(snapshot) {
-            console.log(snapshot.val().numberOfSearches.Users);
-            numUsersSearched = snapshot.val().numberOfSearches.Users;
-            numUsersSearched++;
-            usersRef.child("words").push().set({
-                "words": word
-            });
-           
-            
-            usersRef.child("numberOfSearches").update({
-                "Users": numUsersSearched
-             });
-            
-        }, function(errorObject) {
-            console.log("The read failed: " + errorObject.code);
-    });
-        
-   
-    
+	usersRef.child("numberOfSearches/Users").transaction(function (current_value) {
+		return (current_value || 0) + 1;
+	});
+	usersRef.child("words").push({"words": word});
 }
+
