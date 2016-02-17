a99671ff7afb349dabeb6ab4943171ded648e112
qxzcode <qxzcraft@gmail.com>
2016-02-16 16:19:55 -0800

    Diabled server listening for testing

--- index.js
+++ index.js
@@ -118,9 +118,10 @@ http.createServer(function(request, response) {
     
     response.end();
 
-}).listen(process.env.PORT || 5000);
+});//.listen(process.env.PORT || 5000);
 
 console.log("Port: "+(process.env.PORT || 5000));
+console.log("Ending???");
 
 
 /*
